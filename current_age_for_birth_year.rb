dd75cd930c9c0a0e5e411ff5c79a3c6a339ddd5b
Hayoung Wang <hayoung0803@gmail.com>
2016-08-05 14:46:54 +0000

    Done.

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,11 @@
 def current_age_for_birth_year
   puts "returns the #{age_of_person} based on the #{year_of_birth}"
-  def age_of_person = current_age_for_birth_year(1984)
-    2016-1984
-  end
+end
+
+def age_of_person
+  2016-1984
+end
+
+def year_of_birth(1984)
+  puts 1984
 end
